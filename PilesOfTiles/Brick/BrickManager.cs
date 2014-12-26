d7d6840cdffee1a917e1a09abc57b74b02221347
Simon Søllingvraa <simon@soellingvraa.dk>
2014-12-26 09:28:46 +0100

    Added tile maps

--- PilesOfTiles/Brick/BrickManager.cs
+++ PilesOfTiles/Brick/BrickManager.cs
@@ -43,6 +43,10 @@ namespace PilesOfTiles.Brick
             });
         }
 
+        public void Handle(BrickCollided message)
+        {
+            SpawnRandomBrickAt(_spawnPosition);
+        }
 
         public void Draw(SpriteBatch spriteBatch, Texture2D texture, int tileSize)
         {
@@ -71,50 +75,303 @@ namespace PilesOfTiles.Brick
                         Direction.Up,
                         new List<Tile>
                         {
-                            Tile.Create(new Vector2(0, 1), Color.Blue),
-                            Tile.Create(new Vector2(1, 0), Color.Blue),
-                            Tile.Create(new Vector2(1, 1), Color.Blue),
-                            Tile.Create(new Vector2(2, 1), Color.Blue)
+                            Tile.Create(new Vector2(0, 1), Color.MediumPurple),
+                            Tile.Create(new Vector2(1, 0), Color.MediumPurple),
+                            Tile.Create(new Vector2(1, 1), Color.MediumPurple),
+                            Tile.Create(new Vector2(2, 1), Color.MediumPurple)
                         }
                     },
                     {
                         Direction.Right,
                         new List<Tile>
                         {
-                            Tile.Create(new Vector2(1, 0), Color.Blue),
-                            Tile.Create(new Vector2(1, 1), Color.Blue),
-                            Tile.Create(new Vector2(2, 1), Color.Blue),
-                            Tile.Create(new Vector2(1, 2), Color.Blue)
+                            Tile.Create(new Vector2(1, 0), Color.MediumPurple),
+                            Tile.Create(new Vector2(1, 1), Color.MediumPurple),
+                            Tile.Create(new Vector2(2, 1), Color.MediumPurple),
+                            Tile.Create(new Vector2(1, 2), Color.MediumPurple)
                         }
                     },
                     {
                         Direction.Down,
                         new List<Tile>
                         {
-                            Tile.Create(new Vector2(0, 1), Color.Blue),
-                            Tile.Create(new Vector2(1, 1), Color.Blue),
-                            Tile.Create(new Vector2(2, 1), Color.Blue),
-                            Tile.Create(new Vector2(1, 2), Color.Blue)
+                            Tile.Create(new Vector2(0, 1), Color.MediumPurple),
+                            Tile.Create(new Vector2(1, 1), Color.MediumPurple),
+                            Tile.Create(new Vector2(2, 1), Color.MediumPurple),
+                            Tile.Create(new Vector2(1, 2), Color.MediumPurple)
                         }
                     },
                     {
                         Direction.Left,
                         new List<Tile>
                         {
-                            Tile.Create(new Vector2(1, 0), Color.Blue),
-                            Tile.Create(new Vector2(0, 1), Color.Blue),
-                            Tile.Create(new Vector2(1, 1), Color.Blue),
-                            Tile.Create(new Vector2(1, 2), Color.Blue)
+                            Tile.Create(new Vector2(1, 0), Color.MediumPurple),
+                            Tile.Create(new Vector2(0, 1), Color.MediumPurple),
+                            Tile.Create(new Vector2(1, 1), Color.MediumPurple),
+                            Tile.Create(new Vector2(1, 2), Color.MediumPurple)
                         }
                     }
 
+                }),
+                new BrickMap(new Dictionary<Direction, IEnumerable<Tile>>
+                {
+                    {
+                        Direction.Up,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, 0), Color.Yellow),
+                            Tile.Create(new Vector2(1, 0), Color.Yellow),
+                            Tile.Create(new Vector2(0, 1), Color.Yellow),
+                            Tile.Create(new Vector2(1, 1), Color.Yellow)
+                        }
+                    },
+                    {
+                        Direction.Right,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, 0), Color.Yellow),
+                            Tile.Create(new Vector2(1, 0), Color.Yellow),
+                            Tile.Create(new Vector2(0, 1), Color.Yellow),
+                            Tile.Create(new Vector2(1, 1), Color.Yellow)
+                        }
+                    },
+                    {
+                        Direction.Down,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, 0), Color.Yellow),
+                            Tile.Create(new Vector2(1, 0), Color.Yellow),
+                            Tile.Create(new Vector2(0, 1), Color.Yellow),
+                            Tile.Create(new Vector2(1, 1), Color.Yellow)
+                        }
+                    },
+                    {
+                        Direction.Left,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, 0), Color.Yellow),
+                            Tile.Create(new Vector2(1, 0), Color.Yellow),
+                            Tile.Create(new Vector2(0, 1), Color.Yellow),
+                            Tile.Create(new Vector2(1, 1), Color.Yellow)
+                        }
+                    }
+                }),
+                new BrickMap(new Dictionary<Direction, IEnumerable<Tile>>
+                {
+                    {
+                        Direction.Up,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(-1, 0), Color.LightBlue),
+                            Tile.Create(new Vector2(0, 0), Color.LightBlue),
+                            Tile.Create(new Vector2(1, 0), Color.LightBlue),
+                            Tile.Create(new Vector2(2, 0), Color.LightBlue)
+                        }
+                    },
+                    {
+                        Direction.Right,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, -1), Color.LightBlue),
+                            Tile.Create(new Vector2(0, 0), Color.LightBlue),
+                            Tile.Create(new Vector2(0, 1), Color.LightBlue),
+                            Tile.Create(new Vector2(0, 2), Color.LightBlue)
+                        }
+                    },
+                    {
+                        Direction.Down,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(-1, 0), Color.LightBlue),
+                            Tile.Create(new Vector2(0, 0), Color.LightBlue),
+                            Tile.Create(new Vector2(1, 0), Color.LightBlue),
+                            Tile.Create(new Vector2(2, 0), Color.LightBlue)
+                        }
+                    },
+                    {
+                        Direction.Left,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, -1), Color.LightBlue),
+                            Tile.Create(new Vector2(0, 0), Color.LightBlue),
+                            Tile.Create(new Vector2(0, 1), Color.LightBlue),
+                            Tile.Create(new Vector2(0, 2), Color.LightBlue)
+                        }
+                    }
+                }),
+                new BrickMap(new Dictionary<Direction, IEnumerable<Tile>>
+                {
+                    {
+                        Direction.Up,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, -1), Color.Orange),
+                            Tile.Create(new Vector2(0, 0), Color.Orange),
+                            Tile.Create(new Vector2(0, 1), Color.Orange),
+                            Tile.Create(new Vector2(1, 1), Color.Orange)
+                        }
+                    },
+                    {
+                        Direction.Right,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(-1, 0), Color.Orange),
+                            Tile.Create(new Vector2(0, 0), Color.Orange),
+                            Tile.Create(new Vector2(1, 0), Color.Orange),
+                            Tile.Create(new Vector2(-1, 1), Color.Orange)
+                        }
+                    },
+                    {
+                        Direction.Down,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, -1), Color.Orange),
+                            Tile.Create(new Vector2(0, 0), Color.Orange),
+                            Tile.Create(new Vector2(0, 1), Color.Orange),
+                            Tile.Create(new Vector2(-1, -1), Color.Orange)
+                        }
+                    },
+                    {
+                        Direction.Left,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(-1, 0), Color.Orange),
+                            Tile.Create(new Vector2(0, 0), Color.Orange),
+                            Tile.Create(new Vector2(1, 0), Color.Orange),
+                            Tile.Create(new Vector2(1, -1), Color.Orange)
+                        }
+                    }
+                }),
+                new BrickMap(new Dictionary<Direction, IEnumerable<Tile>>
+                {
+                    {
+                        Direction.Up,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(1, -1), Color.Blue),
+                            Tile.Create(new Vector2(1, 0), Color.Blue),
+                            Tile.Create(new Vector2(1, 1), Color.Blue),
+                            Tile.Create(new Vector2(0, 1), Color.Blue)
+                        }
+                    },
+                    {
+                        Direction.Right,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, 1), Color.Blue),
+                            Tile.Create(new Vector2(1, 1), Color.Blue),
+                            Tile.Create(new Vector2(2, 1), Color.Blue),
+                            Tile.Create(new Vector2(0, 0), Color.Blue)
+                        }
+                    },
+                    {
+                        Direction.Down,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(1, -1), Color.Blue),
+                            Tile.Create(new Vector2(1, 0), Color.Blue),
+                            Tile.Create(new Vector2(1, 1), Color.Blue),
+                            Tile.Create(new Vector2(2, -1), Color.Blue)
+                        }
+                    },
+                    {
+                        Direction.Left,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, 1), Color.Blue),
+                            Tile.Create(new Vector2(1, 1), Color.Blue),
+                            Tile.Create(new Vector2(2, 1), Color.Blue),
+                            Tile.Create(new Vector2(2, 2), Color.Blue)
+                        }
+                    }
+                }),
+                new BrickMap(new Dictionary<Direction, IEnumerable<Tile>>
+                {
+                    {
+                        Direction.Up,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(2, 0), Color.Green),
+                            Tile.Create(new Vector2(1, 0), Color.Green),
+                            Tile.Create(new Vector2(0, 1), Color.Green),
+                            Tile.Create(new Vector2(1, 1), Color.Green)
+                        }
+                    },
+                    {
+                        Direction.Right,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, 0), Color.Green),
+                            Tile.Create(new Vector2(1, 2), Color.Green),
+                            Tile.Create(new Vector2(0, 1), Color.Green),
+                            Tile.Create(new Vector2(1, 1), Color.Green)
+                        }
+                    },
+                    {
+                        Direction.Down,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, 0), Color.Green),
+                            Tile.Create(new Vector2(1, 0), Color.Green),
+                            Tile.Create(new Vector2(0, 1), Color.Green),
+                            Tile.Create(new Vector2(-1, 1), Color.Green)
+                        }
+                    },
+                    {
+                        Direction.Left,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, 0), Color.Green),
+                            Tile.Create(new Vector2(1, 0), Color.Green),
+                            Tile.Create(new Vector2(0, -1), Color.Green),
+                            Tile.Create(new Vector2(1, 1), Color.Green)
+                        }
+                    }
+                }),
+new BrickMap(new Dictionary<Direction, IEnumerable<Tile>>
+                {
+                    {
+                        Direction.Up,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, 0), Color.Red),
+                            Tile.Create(new Vector2(-1, 0), Color.Red),
+                            Tile.Create(new Vector2(0, 1), Color.Red),
+                            Tile.Create(new Vector2(1, 1), Color.Red)
+                        }
+                    },
+                    {
+                        Direction.Right,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, 0), Color.Red),
+                            Tile.Create(new Vector2(1, 0), Color.Red),
+                            Tile.Create(new Vector2(0, 1), Color.Red),
+                            Tile.Create(new Vector2(1, -1), Color.Red)
+                        }
+                    },
+                    {
+                        Direction.Down,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, 0), Color.Red),
+                            Tile.Create(new Vector2(1, 0), Color.Red),
+                            Tile.Create(new Vector2(2, 1), Color.Red),
+                            Tile.Create(new Vector2(1, 1), Color.Red)
+                        }
+                    },
+                    {
+                        Direction.Left,
+                        new List<Tile>
+                        {
+                            Tile.Create(new Vector2(0, 2), Color.Red),
+                            Tile.Create(new Vector2(1, 0), Color.Red),
+                            Tile.Create(new Vector2(0, 1), Color.Red),
+                            Tile.Create(new Vector2(1, 1), Color.Red)
+                        }
+                    }
                 })
             };
-        }
-
-        public void Handle(BrickCollided message)
-        {
-            SpawnRandomBrickAt(_spawnPosition);
         }
     }
 }
