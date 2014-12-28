994218973034cd5bb96c7746158a4cc3c37d9d9c
Simon Søllingvraa <simon@soellingvraa.dk>
2014-12-28 11:48:14 +0100

    Added level and collision messages

---
+++ PilesOfTiles/Level/Messages/DifficultyLevelChanged.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PilesOfTiles.Level.Messages
+{
+    public class DifficultyLevelChanged
+    {
+        public int Value { get; set; }
+    }
+}
---
+++ PilesOfTiles/Level/Messages/GameCompleted.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PilesOfTiles.Level.Messages
+{
+    public class GameCompleted
+    {
+    }
+}
